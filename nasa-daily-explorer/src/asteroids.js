0f4e8b66280194c67fa3bbf156146162b77011b6
Tanjya Akther <Tanjya26@gmail.com>
2025-09-08 14:10:08 +0100

    working on astroid.js to load API

---
+++ nasa-daily-explorer/src/asteroids.js
@@ -0,0 +1,4 @@
+import { $, show, hide, openModal } from './ui.js';
+
+const API_KEY = '2lWc2sGkMOhTHRThafaKdNcYGP9dHqgawJs6jG2Y';
+const ROVER_BASE = `https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=${API_KEY}`;
